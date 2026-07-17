09fb1bf2b9b4ad85da5b1522a83dbeace9525dfe
Nouman Developer <nt50616849@gmail.com>
2026-07-17 18:19:45 +0500

    Updated

--- src/server.js
+++ src/server.js
@@ -30,6 +30,7 @@ const allowedOriginsSet = new Set([
   "http://localhost:5501",
   "http://127.0.0.1:8000",
   "http://127.0.0.1:5501",
+  "http://127.0.0.1:5500",
   "http://127.0.0.1:8080",
   "https://stemy.vercel.app"
 ]);
